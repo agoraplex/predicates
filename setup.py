8ee64725ed41333d55d89cfda3b907976d53ace0
Tripp Lilley <tripplilley@gmail.com>
2013-02-08 15:18:32 -0500

    centralize requirements for reuse within `setup.py`

--- setup.py
+++ setup.py
@@ -3,6 +3,21 @@ use_setuptools()
 
 from setuptools import setup
 
+requirements = {
+    'install': [],
+    'extras': {
+        'docs': [
+            'sphinx>=1.1',
+            'agoraplex.themes.sphinx',
+            ],
+        'tests': [
+            'nose>=1.2.1',
+            'coverage>=3.6',
+            'pinocchio>=0.3.1',
+            'xtraceback>=0.3.3',
+            ],
+        },
+    }
 
 setup(
     name='predicates',
@@ -15,12 +30,7 @@ setup(
     license='BSD',
     description='A collection of predicate factories, functions, and partials, for functional programming.',
     long_description=open('README.rst').read(),
-    install_requires=[],
-    tests_require=['nose>=1.2.1'],
-    extras_require={
-        'docs': [
-            'sphinx>=1.1',
-            'agoraplex.themes.sphinx',
-            ],
-        },
+    install_requires=requirements.get('install', None),
+    tests_require=requirements.get('extras', {}).get('tests', None),
+    extras_require=requirements.get('extras', None),
 )
